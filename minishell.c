5b424c62e27904916a571408aff2d783c8212b07
rohit <rkugver1@binghamton.edu>
2016-02-09 12:41:52 -0500

    get command from user

--- minishell.c
+++ minishell.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+#include <string.h>
+#define INP_SIZE_MAX    1024
 int main(void)
 {
-	
-	printf("ush>");
-	gets
+    char inp[INP_SIZE_MAX];
+    memset(inp,0,sizeof(inp));    
+	while(1)    {
+        printf("msh>");
+        fgets(inp, INP_SIZE_MAX, stdin);
+        printf("%s",inp);
+    }
 	return 0;
 }
