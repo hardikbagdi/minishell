ed0835e26f22b726321fa4c33ea88330729a21c5
rohit <rkugver1@binghamton.edu>
2016-02-13 13:19:20 -0500

    redirection code

--- minishell.c
+++ minishell.c
@@ -5,6 +5,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "minishell.h"
 
 #define INP_SIZE_MAX    1024 //max chars in a cmd
 #define ARGS_MAX 10	// max args per cmd
@@ -18,6 +22,8 @@ struct child_process {
 	pid_t pid;	//pid of this child
 } child_list[CHILD_PER_CMD_MAX];	// a cmd line can have at most
 int num_childs;	// total number of child processes in this command 
+
+pid_t shell_pid;    //Pid of shell
 
 int split_str( char *line, unsigned int line_len, char **tok_list, unsigned int tok_list_len)
 {
@@ -60,27 +66,127 @@ int fill_child_list( void )
         if( *inp_tokens[i] == '|')  {
             j++;
             child_list[j].cmd_start_index = i + 1;
+            child_list[j].cmd_len = 1;
         }else   {
             child_list[j].cmd_len++;
         }
     }
     num_childs = j+1;
+    printf("total number of childs = %d",num_childs);
     return 0;	
 }
 
+int current_child = -1;  //index to the child_list[]
+
+int run_exec(void)
+{
+    char *argv[ARGS_MAX];
+    int i = 0, j = 0;
+    int in_fd;
+    int out_fd;
+    char* in_file = NULL;
+    char* out_file = NULL;
+    
+    memset(argv, 0 , sizeof(argv)); //check this 
+    for( i = child_list[current_child].cmd_start_index; i <child_list[current_child].cmd_len; i++)  {
+        if(*inp_tokens[i] == '>')    {
+            out_file = inp_tokens[i + 1];
+            i++;    //skip the next token because it is a fine name
+        }   
+        if(*inp_tokens[i] == '<')   { 
+            in_file = inp_tokens[i + 1];   
+            i++;    //skip the next token because it is a fine name
+        }
+        else    {
+            argv[j++] = inp_tokens[i];
+        }
+    }
+    if( in_file != NULL )   {
+       if( open(in_file, O_RDONLY ) < 0 )   {
+            printf("Error %s for cmd %s, file %s\n", strerror(errno), argv[0], in_file);
+            return 1;   
+       }
+       if( dup2(in_fd, STDIN_FILENO) < 0)   {
+            printf("Error in dup %s for cmd %s, file %s\n", strerror(errno), argv[0], in_file);
+            return 1;   
+       }
+       close(in_fd);
+    }
+
+    if( out_file != NULL )   {
+       if( open(out_file, O_CREAT|O_WRONLY|O_TRUNC ) < 0 )   {
+            printf("Error %s for cmd %s, file %s\n", strerror(errno), argv[0], out_file);
+            return 1;   
+       }
+        if( dup2(out_fd, STDOUT_FILENO) < 0)   {
+            printf("Error in dup %s for cmd %s, file %s\n", strerror(errno), argv[0], in_file);
+            return 1;   
+       }
+       close(out_fd);
+    }
+
+
+    if( execvp(argv[0], argv) < 0 )  {
+        printf("Error %s for cmd %s\n", strerror(errno), argv[0]);
+    }
+    return 0;
+}
+int spawn_child( void )
+{
+    pid_t pid;
+
+    current_child++;
+    printf("current child = %d", current_child);
+    pid = fork();
+    if( pid < 0 )   {
+        printf("Error1 %s \n", strerror(errno));
+        exit(1);
+    }
+    if( pid == 0 )  {   //child
+        if( current_child < (num_childs - 1))   { // there are more commands in pipe, spawn recursively
+            spawn_child();
+        } else{             // last cmd in pipe
+            printf("last command, %d", current_child);
+            run_exec();
+        }
+    }
+    else    {   //parent
+        if( this_is_shell())    {
+            wait(NULL); //shell does not exec itself
+        }
+        else    {
+            run_exec(); // this is a child
+        }
+    }
+    return 0;
+}
+
+int this_is_shell( void )
+{
+    pid_t this_pid = 0;
+    this_pid = getpid();
+    if( this_pid == shell_pid )
+        return TRUE;
+    else
+        return FALSE;
+}
 int main(void)
 {
     int child_status = 0;
     pid_t pid;
     memset(inp,0,sizeof(inp));    
+    //record the pid of shell before spawining children
+    shell_pid = getpid();
     while(1)    {
         printf("msh>");
         fgets(inp, INP_SIZE_MAX, stdin);
        	//tokanise the cmd line input
     	split_str( inp, sizeof(inp), inp_tokens, sizeof(inp_tokens)); 
 	    //fillout child process array
-	    fill_child_list();	
-	    pid = fork();
+	    fill_child_list();
+        spawn_child();    
+/*
+        pid = fork();
         if( pid < 0 )   {
             printf(" ERROR creating child\n");
             exit(0);
@@ -93,6 +199,7 @@ int main(void)
             waitpid(pid, &child_status, WUNTRACED | WCONTINUED);
             //printf("child done\n");
         }
+*/
         //printf("%s",inp);
     }
 	return 0;
